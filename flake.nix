0c128f27d21f98be549267b37a02b2967cf71d90
NAHO <90870942+trueNAHO@users.noreply.github.com>
2025-02-01 02:57:46 +0100

    flake: packages: application-mongo-{external,local}: init

--- flake.nix
+++ flake.nix
@@ -62,6 +62,23 @@
                     application-default-external = self.application-default;
                     application-default-local = self.application-default;
 
+                    application-mongo-external = self.application-mongo-local;
+
+                    application-mongo-local = pkgs.dockerTools.buildImage {
+                      config.Cmd = ["mongod" "--bind_ip_all"];
+
+                      fromImage = pkgs.dockerTools.pullImage {
+                        finalImageName = "mongo";
+                        finalImageTag = "5.0.31-focal";
+                        hash = "sha256-pnc6tcxAArXZVSbAyLb5RILrpSy/HmUAT+32sA9LGTg=";
+                        imageDigest = "sha256:f7a950ee4a1cc048300cd3b3da3e939974eb8278525101ac917dbed1a3ce3d51";
+                        imageName = "mongo";
+                      };
+
+                      name = "mongo";
+                      tag = "latest";
+                    };
+
                     default = pkgs.buildEnv {
                       name = "default";
 
