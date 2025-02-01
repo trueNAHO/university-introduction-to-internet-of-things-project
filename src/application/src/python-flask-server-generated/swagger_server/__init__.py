66922c2f80294ca2e259349f84d2c977615ab3db
malgaverstehst <malgaverstehst26@gmail.com>
2025-02-01 14:19:15 +0100

    Docker API and database communication

--- src/application/src/python-flask-server-generated/swagger_server/__init__.py
+++ src/application/src/python-flask-server-generated/swagger_server/__init__.py
@@ -5,9 +5,7 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 # Set the MongoDB URI
-app.config["MONGO_URI"] = "mongodb://localhost:27017/Temp_DB"  # Change this to your DB URI
+app.config["MONGO_URI"] = "mongodb://mongodb:27017/Temp_DB"  # Change this to your DB URI
 
 # Initialize the PyMongo object after setting the config
 mongo = PyMongo(app)
-
-
