4fb5adf942d0d45aa5ee7f89bd0d24f5238cf56a
malgaverstehst <malgaverstehst26@gmail.com>
2025-02-02 20:15:58 +0100

    Communication ui and api

--- src/application/src/api/swagger_server/__init__.py
+++ src/application/src/api/swagger_server/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_pymongo import PyMongo
 
 # Initialize the Flask app
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 
 # Set the MongoDB URI
 app.config["MONGO_URI"] = (
@@ -11,3 +14,14 @@ app.config["MONGO_URI"] = (
 
 # Initialize the PyMongo object after setting the config
 mongo = PyMongo(app)
+
+@app.route('/test-cors')
+def test_cors():
+    return jsonify({"message": "CORS test"})
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return response
